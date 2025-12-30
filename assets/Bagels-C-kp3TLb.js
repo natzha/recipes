99332f655b021d594de60bf2c4b4ab21b2f95908
Nathan Zhang <50129222+natzha@users.noreply.github.com>
2025-12-30 22:24:53 +1100

    Deploy to GitHub Pages

---
+++ assets/Bagels-C-kp3TLb.js
@@ -0,0 +1,29 @@
+const n=`# Bagels\r
+\r
+## Notes\r
+Makes 6 large or 8 med. Probs 8 if using sourdough starter instead of yeast. Can consider cold fermenting overnight after bagels have been shaped.  \r
+\r
+## Ingredients\r
+### Main\r
+Yeast (instant dry),  1 pack  \r
+Water,  275g (anywhere 55-60% hydration)  \r
+Salt,  10g (2%)  \r
+Flour,  500g  \r
+Honey or Barley Malt Syrup, 20g  \r
+Baking soda,  1 Tbsp  \r
+Egg,  1  \r
+\r
+### Other\r
+Everything bagel seasoning  \r
+\r
+## Method\r
+1. Bloom yeast with honey  \r
+2. Mix in salt a flour. Knead until just combined. Let rest for 5 mins then knead for 8-10 mins  \r
+3. Let rise until doubled (around 1hr)  \r
+4. Punch down and divide in 6-8 pieces. Shape into boules and let rest for 10 mins  \r
+5. Lightly oil (or dust with cornmeal) a baking tray and shape bagels. Proof until risen (45-60 mins or overnight in fridge)  \r
+6. Boil large pot of water (about 2L) and preheat oven to 250C. Add salt, baking soda and honey (or malt or molasses) to water  \r
+7. Boil each side for 1 min 30 sec and drain over wire rack  \r
+8. Brush with egg wash then put on toppings.  \r
+9. Bake for 15 mins  \r
+10. Cool for 10 mins before cutting  `;export{n as default};
---
+++ assets/Lu Rou Fan-DpU71mQh.js
@@ -0,0 +1,38 @@
+const e=`# Lu Rou Fan  \r
+## Notes\r
+\r
+## Ingredients\r
+### Main\r
+Pork Belly (skin on), 1kg  \r
+Shallots, 4  \r
+Ginger, 5 slices  \r
+Garlic,  half a head  \r
+Spring Onion, 2 stalks  \r
+Light Soy Sauce, 1/3 cup  \r
+Dark Soy Sauce, 1/4 cup  \r
+Chinese Cooking Wine, 1/4 cup  \r
+Bay Leaves, 5  \r
+Cloves, 10  \r
+Star Anise, 3  \r
+Sugar, 1 tablespoon  \r
+Quail Eggs, 2 tins  \r
+Chinese 5 spice powder, 1 teaspoon  \r
+Groud white pepper, 1 teaspoon  \r
+\r
+### Other\r
+Salt, msg, chicken boullion  \r
+\r
+## Method\r
+1. Cut pork belly into 1 cm strips then 1cm pieces  \r
+2. Add pork belly into a pot and add water until barely covering all the meat  \r
+3. Cook on high until all the water evaporates and the fat starts rendering  \r
+4. Switch to med-low and slowly fry to render out the fat from the pork belly. Cook until pork is lightly browned  \r
+5. Slice up the shallots thinly. Once the pork belly is done, remove the pork belly and fry the shallots in the rendered out lard at high heat, switching to low when most of the water is boiled off as it goes from crispy to burnt very quickly after that point  \r
+6. Drain the lard from the shallots and reserve for other dishes  \r
+7. In a braising vessel, add a bit of the shallot infused lard, the pork belly, ginger, garlic and spring onion. Fry for a minute  \r
+8. Add in light soy sauce, dark soy sauce, cooking wine and fried shallots  \r
+9. Add in bay leaves, cloves, star anise, sugar, 5 spice powder, ground white pepper and quail eggs.  \r
+10. Add water until liquid covers all the meat and braise for 1 hour on med heat with the lid on. Top up water as necessary  \r
+11. Once the braising is done and meat is soft, adjust seasoning as necessary with salt, msg, chicken boullion, and any of the previously added sauces and spices.  \r
+12. Serve over rice  \r
+`;export{e as default};
